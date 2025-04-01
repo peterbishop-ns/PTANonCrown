35a55c2d5ed2ea9985fae3780e943e67c078cbe3
peterbishop-ns <168752624+peterbishop-ns@users.noreply.github.com>
2025-04-01 14:38:23 -0300

    Tab and Enter move along

--- PTANonCrown/Behaviours/EntryFormBehaviour.cs
+++ PTANonCrown/Behaviours/EntryFormBehaviour.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using PTANonCrown.Models;
 using PTANonCrown.ViewModel;
 
-
 namespace PTANonCrown.Behaviours
 {
     public class EntryFormBehaviour : Behavior<Entry>
@@ -15,47 +14,58 @@ namespace PTANonCrown.Behaviours
         protected override void OnAttachedTo(Entry bindable)
         {
             base.OnAttachedTo(bindable);
-            bindable.Completed += OnCompleted;
-        }
-
-        private void OnCompleted(object? sender, EventArgs e)
-        {
-            if (sender is not Entry entry)
-                return;
-
-            // Find parent CollectionView
-            var parentCollection = entry.FindParent<CollectionView>();
-            if (parentCollection == null)
-                return;
-
-            // Find all entries inside CollectionView
-            var entries = parentCollection
-                .FindDescendants<Entry>()
-                .ToList();
-
-          //  if (entries.Count == 0)
-          //
-            // Find the index of the current Entry
-            int currentIndex = entries.IndexOf(entry);
-
-            if (currentIndex >= 0 && currentIndex < entries.Count - 1)
-            {
-                // Move focus to next Entry
-                entries[currentIndex + 1].Focus();
-            }
-            else
-            {
-                // Optionally, handle wrap-around (focus back to first Entry)
-                entries[0].Focus();
-            }
+            bindable.Completed += OnCompleted; // Handles Enter key
+            bindable.HandlerChanged += OnHandlerChanged;
         }
 
         protected override void OnDetachingFrom(Entry bindable)
         {
             base.OnDetachingFrom(bindable);
             bindable.Completed -= OnCompleted;
+            bindable.HandlerChanged -= OnHandlerChanged;
         }
 
+        private void OnHandlerChanged(object? sender, System.EventArgs e)
+        {
+#if WINDOWS
+        if (sender is Entry entry && entry.Handler?.PlatformView is Microsoft.UI.Xaml.Controls.TextBox nativeEntry)
+        {
+            nativeEntry.KeyDown += (s, args) => OnKeyDown(entry, args);
+        }
+#endif
+        }
 
+#if WINDOWS
+    private void OnKeyDown(Entry entry, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+    {
+        if (e.Key == Windows.System.VirtualKey.Tab)
+        {
+            e.Handled = true;
+            MoveFocus(entry);
+        }
+    }
+#endif
+
+        private void OnCompleted(object? sender, System.EventArgs e)
+        {
+            if (sender is Entry entry)
+            {
+                MoveFocus(entry);
+            }
+        }
+
+        private void MoveFocus(Entry currentEntry)
+        {
+            var parentCollection = currentEntry.FindParent<CollectionView>();
+            if (parentCollection == null) return;
+
+            var entries = parentCollection.FindDescendants<Entry>().ToList();
+            int currentIndex = entries.IndexOf(currentEntry);
+
+            if (currentIndex >= 0 && currentIndex < entries.Count - 1)
+            {
+                entries[currentIndex + 1].Focus();
+            }
+        }
     }
 }
